132b61cc0dfcd7deb1ca8cf97017362ae973bf39
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-04-14 16:13:14 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+    createTree(ul, data[key]);
+  }
+}
+
+createTree(tree, food);
